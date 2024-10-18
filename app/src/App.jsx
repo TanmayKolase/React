c91980666149e0d6596c3a40cdb0470a21fdc1c7
TanmayKolase <tanmay.kolase@p99soft.com>
2024-10-19 00:08:00 +0530

    Add routing to navigate between components from App

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,39 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 import "./App.css";
-import Counter from "./components/Counter";
 import Greeting from "./components/Greeting";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <>
       <h1>Welcome to the React App</h1>
-      {/* <Greeting name="Tanmay" age={20} /> */}
-      <Counter />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/greeting">Greeting</NavLink>
+              </li>
+              <li>
+                <NavLink to="/counter">Counter</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/greeting" element={<Greeting name="Tanmay" />} />
+            <Route path="/counter" element={<Counter />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- app/src/components/Greeting.jsx
+++ app/src/components/Greeting.jsx
@@ -4,7 +4,6 @@ const Greeting = ({ name, age }) => {
   return (
     <>
       <h1>You are champion {name}</h1>
-      <p>And You are just {age} years old</p>
     </>
   );
 };
